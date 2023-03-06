9339202d263eb726d08349acb0e905e4971764e2
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-03-06 13:43:42 -0800

    configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    allowedHosts: 'all',
+  },
   entry: {
     solution: path.resolve(__dirname, './src/main/models/Stacker.ts'),
     challenge: path.resolve(__dirname, './static/challenge.js'),
   },
   output: {
+    publicPath: '/',
     path: path.resolve(__dirname, './dist'),
     filename: '[name].[fullhash:8].js',
     sourceMapFilename: '[name].[fullhash:8].map',
@@ -16,6 +21,9 @@ module.exports = {
     clean: true,
   },
   resolve: {
+    alias: {
+      static: path.resolve(__dirname, './static'),
+    },
     extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
@@ -23,14 +31,28 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'swc-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.png/,
+        include: path.resolve(__dirname, './static'),
         type: 'asset',
+      },
+      {
+        // Exposes jQuery for use outside Webpack build
+        test: require.resolve('jquery'),
+        loader: 'expose-loader',
+        options: {
+          exposes: ['$', 'jQuery'],
+        },
       },
     ],
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      'window.$': 'jquery',
+      'window.jQuery': 'jquery',
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       template: path.resolve(__dirname, './static/challenge.ejs'),
